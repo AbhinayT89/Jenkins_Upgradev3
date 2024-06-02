0c8f63a808b9298197be4ee489a184ebdff8d18a
AbhinayT89 <166503493+AbhinayT89@users.noreply.github.com>
2024-06-02 15:23:31 +0530

    Removing Extra Testcase

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -20,15 +20,8 @@ public class TestGreeter {
     greeter = new Greeter();
   }
 
-  @Test
-  public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone), containsString(someone));
-  }
-  
-
-  @Test
+}
+@Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
